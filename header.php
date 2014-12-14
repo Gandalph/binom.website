26b03061f95f75e015b3740a9da1beebdcf07dc0
trznjak <djolevasojevic@gmail.com>
2014-12-14 23:10:19 +0100

    svasta

--- header.php
+++ header.php
@@ -24,10 +24,10 @@
     <div id="slide-top" title="Vrh"></div>
 
     <header>
-		<img class="header-image" src="slike/logo-left.jpg" alt="logo-left" height="300" style="float:left;" > 
+		<img class="header-image" src="slike/logo-left.jpg" alt="logo-left" height="300px" style="float:left;" > 
         <div id="logo">
         </div><!-- end logo -->
-        <img class="header-image" src="slike/logo-right.jpg" alt="logo-right" height="300" > 
+        <img class="header-image" src="slike/logo-right.jpg" alt="logo-right" height="300px" > 
         <div id="nav-wrapper">
             <nav id="nav">
                 <!--
