5861f4f29bbd2e8a51285e166317e6fca128b811
trznjak <djolevasojevic@gmail.com>
2014-12-18 15:23:50 +0100

    arhiva

--- arhiva.php
+++ arhiva.php
@@ -2,6 +2,8 @@
 <?php include("header.php"); ?>
 
     <main id="content" style="text-align: center;">
-        <a href="arhiva/BINOM_2014.pdf"><img style="width: 400px;" class="pdf" height="600px" alt="naslovna strana" src="./arhiva/naslovna.jpg" ?></a>
-
+	<div data-configid="14810493/10587493" style="width:525px; height:371px;" class="issuuembed"></div><script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script> 
+    <div data-configid="14810493/10570157" style="width:525px; height:367px;" class="issuuembed"></div><script type="text/javascript" src="//e.issuu.com/embed.js" async="true"></script>     
+        
+        
 <?php include("footer.php"); ?>
