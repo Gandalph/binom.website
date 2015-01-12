2a8a3e361fbb71c11ed9960b0ed4c50b7a4eaf97
Gandalf <gandalph_92@live.com>
2015-01-12 18:06:02 +0100

    Merge branch 'master' of github.com:Gandalph/binom.website
    Conflicts:
    	css/index.css

--- galerija.php
+++ galerija.php
@@ -3,30 +3,55 @@
 
     <div id="content-wrapper">
         <main id="content">
-			<style>
-				img {
-						float:left;
-					}
-			</style>
-	<div>
 		<?php
-			function fetchData($url){
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			return $result;
-			}
-			$result = fetchData("https://api.instagram.com/v1/users/self/feed?access_token=1645696403.c2a8340.63726c5a58f84a748101b2d7a42f19bf");
-			$result = json_decode($result);
-				$i = 0;
-			foreach ($result->data as $post) {
-				$i++;
-				$string = $post->images->thumbnail->url;
-				echo  "<img  src=\"" . $string . "\" />";
-			}
-			?>  
-	</div>
+
+
+function curl_download($Url){
+
+	// is cURL installed yet?
+	if (!function_exists('curl_init')){
+		die('Sorry cURL is not installed!');
+	}
+
+	// OK cool - then let's create a new cURL resource handle
+	$ch = curl_init();
+
+	// Now set some options (most are optional)
+
+	// Set URL to download
+	curl_setopt($ch, CURLOPT_URL, $Url);
+
+	// User agent
+	curl_setopt($ch, CURLOPT_USERAGENT, "MozillaXYZ/1.0");
+
+	// Include header in result? (0 = yes, 1 = no)
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+
+	// Should cURL return or print out the data? (true = return, false = print)
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	// Timeout in seconds
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+	// Download the given URL, and return output
+	$output = curl_exec($ch);
+
+	// Close the cURL resource, and free system resources
+	curl_close($ch);
+
+	return $output;
+}
+
+$result = curl_download("https://api.instagram.com/v1/users/self/feed?access_token=1645696403.c2a8340.63726c5a58f84a748101b2d7a42f19bf");
+$result = json_decode($result);
+$i = 0;
+foreach ($result->data as $post) {
+	$i++;
+	$string = $post->images->thumbnail->url;
+	echo "<img src=\"" . $string . "\" />";
+}
+
+?>
 <?php include("footer.php"); ?>
