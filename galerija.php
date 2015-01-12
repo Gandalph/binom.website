22a8a465ba646780d208f4f930bd8c0f7abeaa4a
trznjak <djolevasojevic@gmail.com>
2015-01-12 23:26:06 +0100

    bla

--- galerija.php
+++ galerija.php
@@ -2,39 +2,13 @@
 <?php include("./baza/db.inc"); ?>
 <?php include("header.php"); ?>
 
-<<<<<<< HEAD
 	<style>
 		.thumbnail{
-				margin:10px;
+				margin:17px;
 				float:left;
 			}
 	</style>
-	<div id="content-wrapper">
-		<main id="content" >
-			<?php
-				function fetchData($url){
-				$ch = curl_init();
-				curl_setopt($ch, CURLOPT_URL, $url);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-				$result = curl_exec($ch);
-				curl_close($ch);
-				return $result;
-				}
-				$result = fetchData("https://api.instagram.com/v1/users/self/feed?access_token=1645696403.c2a8340.63726c5a58f84a748101b2d7a42f19bf");
-				$result = json_decode($result);
-					$i = 0;
-				foreach ($result->data as $post) {
-					$i++;
-					$string = $post->images->thumbnail->url;
-					echo  "<img class=\"thumbnail\"  src=\"" . $string . "\" />";
-				}
- 			?>  
-			
-			
-			
-        
-=======
+
     <div id="content-wrapper">
         <main id="content">
 		<?php
@@ -84,9 +58,9 @@ $i = 0;
 foreach ($result->data as $post) {
 	$i++;
 	$string = $post->images->thumbnail->url;
-	echo "<img src=\"" . $string . "\" />";
+	echo "<img class=\"thumbnail\" src=\"" . $string . "\" />";
 }
 
 ?>
->>>>>>> 709b8f6d388181d3e5eb6540ea9e993ad8d99140
+
 <?php include("footer.php"); ?>
