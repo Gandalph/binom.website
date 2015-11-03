c4d04b477eaed941de36fa725ed031db6f91d068
trznjak <djolevasojevic@gmail.com>
2015-11-03 22:29:17 +0100

    ...

--- onama.php
+++ onama.php
@@ -4,7 +4,7 @@
 	<div id="content-wrapper">
 		<main id="content" style="height:400px;">
 			<div style="width:750px; height:5px; border-bottom: 2px solid #666;  margin: auto; margin-top: 50px; "></div> <!--TODO prebaci u CSS-->
-                            <font color="gray" face="arial black" size="8px"><h4 style="text-align: center">...у припреми</h4></font>
+                            <font color="gray" face="arial black" size="8px"><h4 style="text-align: center">...у припреми...</h4></font>
 			</div>
         
 <?php include("footer.php"); ?>
